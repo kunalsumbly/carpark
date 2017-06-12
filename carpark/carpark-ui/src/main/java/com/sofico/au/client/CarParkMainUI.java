7808375008128eb5e63f9dc18c9770122645407e
kunalsumbly <kunal.sumbly@gmail.com>
2017-06-12 23:14:38 +1000

    This is the first commit to for carpark project with Spring Boot and Vaadin

---
+++ carpark/carpark-ui/src/main/java/com/sofico/au/client/CarParkMainUI.java
@@ -0,0 +1,30 @@
+package com.sofico.au.client;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+/**
+ * This class is the entry point of car park application
+ * @author kusu
+ *
+ */
+@SpringUI(path="/carpark")
+@Title("S S A C A R P A R K")
+@Theme("valo")
+public class CarParkMainUI extends UI{
+	
+
+	@Override
+	protected void init(VaadinRequest request) {
+		VerticalLayout rootLayout = new VerticalLayout();
+		rootLayout.addComponent(new Label("This is the test carpark"));
+		
+		setContent(rootLayout);
+		
+	}
+
+}
---
+++ carpark/carpark-web/src/main/java/com/sofico/au/SpringBootApplication.java
@@ -0,0 +1,28 @@
+package com.sofico.au;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration // is the used to register bean and enable annotations is same as defining a bean in xml and using <context:annotation-config/>
+ //to enable the annotation 
+@EnableAutoConfiguration//Spring Boot auto-configuration attempts to automatically configure your Spring application based on the jar dependencies that you have added
+@ComponentScan({"com.au.sofico"})// seems same as <context:component-scan> which not only enables the annotations but also registers the bean in the context or IOC container
+//@EnableJpaRepositories({"com.au.sofico"}) // specific to JPA repositories
+//@EntityScan({"com.au.sofico"})// JPA entity scan
+public class SpringBootApplication extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootApplication.class);
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class,args);
+	}
+}
